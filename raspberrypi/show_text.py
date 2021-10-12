4e8a0a19cd0386ab4865c1177b124befecc24125
zhanzghiyong0410 <2556703570@qq.com>
2021-10-12 16:38:48 +0800

    delete

--- raspberrypi/show_text.py
+++
@@ -1,9 +0,0 @@
-from rgb_matrix import RGB_Matrix
-import numpy as np
-import time 
-
-if __name__ == "__main__":
-    rr = RGB_Matrix(0X74)
-
-    while True:
-        rr.show_text("  Hello",delay=200,color=(0,51,128))
